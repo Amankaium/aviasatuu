58cf3560da8f66e411bb3b4a58aa445aab0c2700
unknown <arapbaeva@github.com>
2022-05-09 20:32:57 -0600

    add components as AccountMenu and LanguageMenu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import Home from './pages/home/Home';
 import Flights from './pages/flights/Flights';
 import Passengers from './pages/passengers/Passengers';
 import Error from './pages/error/Error';
 import CheckoutPage from './pages/checkoutpage/CheckoutPage';
+// import Account from './components/account/Account';
+import LanguageMenu from './components/language/LanguageMenu';
+import AccountMenu from './components/account/AccountMenu';
 
 function App() {
+	// const [modalActive, setModalActive] = useState(true)
 	return (
 		<div className='container'>
 			<Router>
+			<AccountMenu />
+		    <LanguageMenu />
 				<Switch>
 					<Route exact path='/' component={Home} />
 					<Route path='/flights' component={Flights} />
--- src/components/language/Language.js
+++ src/components/language/Language.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './Language.css';
+// import Vector from '../../../src/assets/icons/Vector.svg';
 
-const Language = () => {
-	return <div>Language</div>;
+
+const Language = ({active, setActive, children}) => {
+    return (
+	
+		
+
+
+        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>	
+		
+            <div className={active ? "modal__content" : "modal__content"} onClick={e => e.stopPropagation()}>
+			{children}
+            </div>
+
+        </div>
+    );
 };
 
 export default Language;
