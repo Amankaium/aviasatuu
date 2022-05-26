1fda3f098ace1804dbaaaaa049aff74acdaee0d2
Aiia <ainzileva@gmail.com>
2022-05-27 02:11:00 +0600

    added new component FlightsContext.js and added Context Api, added data from api,change flights item from static to dynamic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { FlightsProvider } from './FlightsContext';
 
 import Home from './pages/home/Home';
 import Flights from './pages/flights/Flights';
@@ -14,17 +15,19 @@ function App() {
 	return (
 		<div className='container'>
 			<Router>
-				<Navbar />
-				<Routes>
-					<Route path='/' element={<App />} />
-					<Route index element={<Home />} />
-					<Route path='flights' element={<Flights />} />
-					<Route path='passengers' element={<Passengers />} />
-					<Route path='/checkout' element={<CheckoutPage />} />
-					<Route path='signin' element={<SignIn />} />
-					<Route path='signup' element={<SignUp />} />
-					<Route path='*' element={<Error />} />
-				</Routes>
+				<FlightsProvider>
+					<Navbar />
+					<Routes>
+						<Route path='/' element={<App />} />
+						<Route index element={<Home />} />
+						<Route path='flights' element={<Flights />} />
+						<Route path='passengers' element={<Passengers />} />
+						<Route path='/checkout' element={<CheckoutPage />} />
+						<Route path='signin' element={<SignIn />} />
+						<Route path='signup' element={<SignUp />} />
+						<Route path='*' element={<Error />} />
+					</Routes>
+				</FlightsProvider>
 			</Router>
 		</div>
 	);
--- src/components/flightsitem/FlightsItem.js
+++ src/components/flightsitem/FlightsItem.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import FlightsContext from '../../FlightsContext';
 import planeIcon from '../../assets/icons/icon_plane.svg';
 import './FlightsItem.css';
 
-const FlightsItem = () => {
+const FlightsItem = ({
+	aviacompany,
+	duration,
+	from_city,
+	to_city,
+	start,
+	end,
+	price,
+}) => {
+	const { code_city_maker, getDateHours } = useContext(FlightsContext);
 	return (
 		<div className='flightsItem__inner'>
 			<div className='flightsItem__times flightsItem__inner-element'>
 				<p className='flightsItem__time sub-title'>
-					<span className='caption-text'>FRU</span>
-					05:50
+					<span className='caption-text'>{code_city_maker(from_city)}</span>
+					{getDateHours(new Date(start))}
 				</p>
 				<img src={planeIcon} alt='icon of the plane' />
 				<p className='flightsItem__time sub-title'>
-					<span className='caption-text'>NYC</span>
-					17:05
+					<span className='caption-text'>{code_city_maker(to_city)}</span>
+					{getDateHours(new Date(end))}
 				</p>
 			</div>
 
@@ -23,11 +33,12 @@ const FlightsItem = () => {
 			</p>
 
 			<div className='flightsItem__airlines flightsItem__inner-element'>
-				<p className='flightsItem__airline'>Uzbekistan Airways</p>
-				<p className='flightsItem__airline'>Air Traffic Company</p>
+				<p className='flightsItem__airline'>{aviacompany}</p>
 			</div>
 
-			<p className='flightsItem__price flightsItem__inner-element'>778$</p>
+			<p className='flightsItem__price flightsItem__inner-element'>{`${price
+				.toFixed(2)
+				.replace(/\d(\d{3})+\)/g, '$& ')}$`}</p>
 
 			<Link
 				className='fligthsItem-btn caption-text flightsItem__inner-element'
